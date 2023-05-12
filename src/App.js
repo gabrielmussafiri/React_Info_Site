126443646c47d34e42a36d725df3659eb18b20b0
gabrielmussafiri <musgabriel2@gmail.com>
2023-05-12 09:35:17 +0200

    style navbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
+import Navbar from "./component/Navbar";
 import "./App.css";
+import Main from "./component/Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,3 +1,10 @@
 function Navbar() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <img src="./images/react-icon-small.png" alt="" className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
+export default Navbar;
