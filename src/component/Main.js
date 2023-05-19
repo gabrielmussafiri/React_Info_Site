3fad8ca584ee1df3742557d47e80badb3f14db90
gabrielmussafiri <musgabriel2@gmail.com>
2023-05-19 11:32:59 +0200

    main component building

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,4 +1,15 @@
 function Main() {
-  return <h1>Main Component</h1>;
+  return (
+    <main className="main--container">
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
+        <li>Was first release in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of entreprise apps , including obile apps </li>
+      </ul>
+    </main>
+  );
 }
 export default Main;
